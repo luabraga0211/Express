a0826aab1c1cd6e1d8764a5f1c93e80a95c3fe2f
luabraga0211 <141754027+luabraga0211@users.noreply.github.com>
2024-09-30 12:05:22 -0300

    segundo commit
    2

--- Aula1/index.js
+++ Aula1/index.js
@@ -3,9 +3,35 @@ const app = express()
 const port = 3000  //Variavel ambiente
 
 const path = require("path")
-const basepath = path.join(__dirname, 'Aula1')
+const basepath = path.join(__dirname) 
 
-app.get('/users/:id', (req, res) => { //get=retorna um valor
+//ler o body
+app.use(
+    express.urlencoded({ //Converter para formato URL
+    extentded: true, // copiar parametros dos objetos
+
+    }), 
+)
+
+app.use(express.json())
+
+var checkAuth = function (req, res, next) {
+    req.authStatus = true
+
+    if (req.authStatus) {
+        console.log('Está logado, pode continuar')
+        next()
+    }   else {
+        console.log('Não esta logado, faça o login para continuar')
+    }
+}
+
+    app.use(checkAuth)
+    app.get('users/add', (req, res) =>{
+        res.sendFile(`${basePath}/form.html`)
+    })
+
+    app.get('/users/:id', (req, res) => { //get=retorna um valor
     const id = req.params.id //resgatar parametros da url
     console.log(`Estamos buscando o usuario: ${id}`) //resgatar o usuario do banco
     res.sendFile(`${basepath}/users.html`)//sendFile=Transferir arquivos. basepath=especifica a pasta raiz.
