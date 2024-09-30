64a877e418e366e7a894ccf60876374a79d441e0
luabraga0211 <141754027+luabraga0211@users.noreply.github.com>
2024-09-30 10:51:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000  //Variavel ambiente
+
+const path = require("path")
+const basepath = path.join(__dirname, 'Aula1')
+const checkAuth = function(req,res,next){
+    req.authStatus = true
+    if(req.authStatus){
+        console.log('Está logado, pode continuar')
+    } else {
+        console.log('Não está logado faça o login')
+    }
+}
+
+app.use(checkAuth)
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basepath}/index.html`)
+
+})
+
+app.listen(port, () => {
+    console.log(`App esta rodando na porta ${port}`)
+})  
