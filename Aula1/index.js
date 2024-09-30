e4c1cc24e378879517b1f326560b4edaaa112ecf
luabraga0211 <141754027+luabraga0211@users.noreply.github.com>
2024-09-30 11:19:51 -0300

    commit
    1

--- Aula1/index.js
+++ Aula1/index.js
@@ -5,7 +5,12 @@ const port = 3000  //Variavel ambiente
 const path = require("path")
 const basepath = path.join(__dirname, 'Aula1')
 
-app.get()
+app.get('/users/:id', (req, res) => { //get=retorna um valor
+    const id = req.params.id //resgatar parametros da url
+    console.log(`Estamos buscando o usuario: ${id}`) //resgatar o usuario do banco
+    res.sendFile(`${basepath}/users.html`)//sendFile=Transferir arquivos. basepath=especifica a pasta raiz.
+    
+})
 
 app.get('/', (req, res) => {
     res.sendFile(`${basepath}/index.html`)
